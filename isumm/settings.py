b40cb0f457e7fd01a647313199b3b55cf88fde71
safuh <harrysafu@gmail.com>
2023-06-29 10:27:46 +0300

    deployment configurations

--- isumm/settings.py
+++ isumm/settings.py
@@ -128,8 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS=['summ/static',]
 # Default primary key field type
+STATIC_ROOT = os.path.join(BASE_DIR , "staticfiles")
+
+
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
