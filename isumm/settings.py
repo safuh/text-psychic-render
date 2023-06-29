f5039ebc9f85ca9a12475eeb35fac200e3972959
safuh <harrysafu@gmail.com>
2023-06-29 10:45:49 +0300

    deployment configurations

--- isumm/settings.py
+++ isumm/settings.py
@@ -20,11 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=cldztbc4jg&xl0!x673!*v2_=p$$eu)=7*f#d0#zs$44xx-h^'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app','localhost']
+DEBUG = 'RENDER' not in os.environ
+ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
@@ -128,8 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-#STATICFILES_DIRS=[os.path.join(BASE_DIR , 'summ/static'),]
+STATIC_URL = '/static/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR , 'summ/static'),]
 # Default primary key field type
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR , "staticfiles")
