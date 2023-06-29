d8dcb280bd566d3b25682283989facc8d0db33a4
safuh <harrysafu@gmail.com>
2023-06-29 10:33:04 +0300

    deployment configurations

--- isumm/settings.py
+++ isumm/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=['/summ/static',]
+STATICFILES_DIRS=[os.path.join(BASE_DIR , 'summ/static'),]
 # Default primary key field type
 STATIC_ROOT = os.path.join(BASE_DIR , "staticfiles")
 
