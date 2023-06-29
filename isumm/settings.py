3e55749491c0c21ce1751a197391f5aeadb6af60
safuh <harrysafu@gmail.com>
2023-06-29 10:39:17 +0300

    deployment configurations

--- isumm/settings.py
+++ isumm/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,9 +129,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR , 'summ/static'),]
+#STATICFILES_DIRS=[os.path.join(BASE_DIR , 'summ/static'),]
 # Default primary key field type
-STATIC_ROOT = os.path.join(BASE_DIR , "staticfiles")
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR , "staticfiles")
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
