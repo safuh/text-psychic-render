2b0ca57174f35cf199a16ac31880e0b3490322ef
safuh <harrysafu@gmail.com>
2023-08-31 22:11:09 +0300

    adds

--- isumm/settings.py
+++ isumm/settings.py
@@ -44,6 +44,10 @@ INSTALLED_APPS = [
     'tpsychicapp',
     'rest_framework',
     "rest_framework_api_key",
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 ]
 
 MIDDLEWARE = [
@@ -146,7 +150,19 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend'
 ]
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
 SITE_ID = 2
 
 PAYPAL_CLIENT_ID='AWmyihi1ip94nQebSLHuF1f8Lw18KL8-HLQX32NgICo4ZFQepQwIN195664eoEUWACxjtMy5C8Jq2G9W'
